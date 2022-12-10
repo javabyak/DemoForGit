c6d0ce1d3190313c898f2c93dd8defe92d62d56f
Akshay <Akshay@DESKTOP-EE065FJ>
2022-12-10 14:30:06 +0530

    next commit

---
+++ src/main/java/in/nareshit/raghu/runner/Runnertest.java
@@ -0,0 +1,13 @@
+package in.nareshit.raghu.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Runnertest implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hellow");
+
+	}
+
+}
